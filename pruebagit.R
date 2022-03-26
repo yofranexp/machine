8a85abc5462309452c9e4e7e9857d8ebaefac8a9
yofranexp <yofran795@gmail.com>
2022-03-26 15:50:20 -0400

    grafico

--- pruebagit.R
+++ pruebagit.R
@@ -1,6 +1,12 @@
 hola <- rep(30, 50)
 
 library(ggplot2)
-
+library(dplyr)
 # Analisis exploratorio 
+
+normal <- rnorm(50, mean = 30, sd = 2)
+binomial <- rbinom(50,12, 0.06)
+
+datos <- as.data.frame(cbind(normal, binomial))
+View(datos)
 
